0373b52ef69758982ba38e99ccfe78b6d53b8bc8
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-09 12:42:48 -0300

    first commit

---
+++ App/todolist/src/test/java/br/com/lucasdantas/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasdantas.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
