d3fd923aa8162dedd016820ec75896b6d7625247
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-10 17:42:58 -0300

    adding getters and setters

--- App/todolist/src/main/java/br/com/lucasdantas/todolist/user/UserModel.java
+++ App/todolist/src/main/java/br/com/lucasdantas/todolist/user/UserModel.java
@@ -1,7 +1,26 @@
 package br.com.lucasdantas.todolist.user;
 
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    public void setUserName(String username){
+        this.username = username;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getUserName(){
+        return this.username;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getPassword(){
+        return this.password;
+    }
 }
