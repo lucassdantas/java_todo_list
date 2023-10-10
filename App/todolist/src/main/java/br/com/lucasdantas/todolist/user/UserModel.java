8076d682e83684add48a1e72d1759868cdeb26df
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-10 18:16:07 -0300

    using h2 database

--- App/todolist/src/main/java/br/com/lucasdantas/todolist/user/UserModel.java
+++ App/todolist/src/main/java/br/com/lucasdantas/todolist/user/UserModel.java
@@ -1,26 +1,10 @@
 package br.com.lucasdantas.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    public void setUserName(String username){
-        this.username = username;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
-    public String getUserName(){
-        return this.username;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public String getPassword(){
-        return this.password;
-    }
 }
