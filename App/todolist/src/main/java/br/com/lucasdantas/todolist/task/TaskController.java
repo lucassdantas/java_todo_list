05c520b1178695090e954985c579055db117c3c7
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-12 09:27:12 -0300

    need to fix list tipping

--- App/todolist/src/main/java/br/com/lucasdantas/todolist/task/TaskController.java
+++ App/todolist/src/main/java/br/com/lucasdantas/todolist/task/TaskController.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.apache.coyote.Response;
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +27,22 @@ public class TaskController {
        taskModel.setIdUser((UUID) request.getAttribute("idUser"));
        
        var currentDate = LocalDateTime.now();
+       
        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
-         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser maior que a data atual");
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / término deve ser maior que a data atual");
        }
+
+      if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor que a data de término");
+       }
+
        var task = this.taskRepository.save(taskModel);
        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+      var tasks = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
+      return tasks;
+   }
 }
