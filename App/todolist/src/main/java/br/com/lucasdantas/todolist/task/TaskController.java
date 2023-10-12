f1a83b17db662f6a1d206add5583276545f08c50
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-12 12:29:52 -0300

    add id to param amnd update by id

--- App/todolist/src/main/java/br/com/lucasdantas/todolist/task/TaskController.java
+++ App/todolist/src/main/java/br/com/lucasdantas/todolist/task/TaskController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,8 +43,16 @@ public class TaskController {
     }
 
     @GetMapping("/")
-    public List<TaskModel> list(HttpServletRequest request){
+    public java.util.List<TaskModel> list(HttpServletRequest request){
       var tasks = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
       return tasks;
+   }
+
+   @PutMapping("/{id}")
+   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+      var idUser = (UUID) request.getAttribute("idUser");
+      taskModel.setIdUser(idUser);
+      taskModel.setId(id);
+      return this.taskRepository.save(taskModel);
    }
 }
