e39fd978eabf8ad65a8233ff76e664b5e0b00efd
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-13 12:34:21 -0300

    adding restriction to task updates by owner user

--- App/todolist/src/main/java/br/com/lucasdantas/todolist/task/TaskController.java
+++ App/todolist/src/main/java/br/com/lucasdantas/todolist/task/TaskController.java
@@ -50,9 +50,20 @@ public class TaskController {
    }
 
    @PutMapping("/{id}")
-   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+   public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
       var task = this.taskRepository.findById(id).orElse(null);
+
+      if (task == null){
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontradas");
+      }
+
+      var idUser = request.getAttribute("idUser");
+      
+      if(!task.getIdUser().equals(idUser)){
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não tem autorização para isso");
+      }
       Utils.copyNonNUllProperties(taskModel, task);
-      return this.taskRepository.save(task);
+      var taskUpdated = this.taskRepository.save(task);
+      return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
    }
 }
